7730e46d145d8961f4f16e955f21374c2262b175
kmp0223 <68722153+kmp0223@users.noreply.github.com>
2025-04-27 22:17:04 -0500

    Update BasCoD.R

--- code/functions/BasCoD.R
+++ code/functions/BasCoD.R
@@ -1,14 +1,14 @@
 library(MASS)
 
 
-BasCoD=function(R_0,R_j_list,embedding_0,embedding_j_list,eps=0.3,X_0,X_j_list){
+BasCoD=function(X_0,X_j_list,R_0,R_j_list,embedding_0,embedding_j_list,eps=0.3){
   p=dim(X_0)[2]
   
   K=length(X_j_list)
   embedding_0=embedding_0[,1:R_0]    
   
   lm0=lm(X_0~-1+embedding_0)
-  Gamma0hat=lm0$coefficients %>%t    
+  Gamma0hat=t(lm0$coefficients)
   
   pvals=c()    
   
@@ -19,8 +19,8 @@ BasCoD=function(R_0,R_j_list,embedding_0,embedding_j_list,eps=0.3,X_0,X_j_list){
     embedding_j=embedding_j_list[[k]]
     embedding_j=embedding_j[,1:R_j]        
     
-    lma=lm(X_j~-1+embedding_j)
-    Gammajhat=lma$coefficients %>%t
+    lmj=lm(X_j~-1+embedding_j)
+    Gammajhat=t(lmj$coefficients)
     Gammajhat=apply(Gammajhat,2,function(x)x/norm(x,"2"))    
     
     if(R_j!=1){
@@ -31,5 +31,8 @@ BasCoD=function(R_0,R_j_list,embedding_0,embedding_j_list,eps=0.3,X_0,X_j_list){
     pvals=c(pvals,pchisq(sum(-2*log(pnorm((atanh(rho)-atanh(1-eps))*sqrt(p-3)))),lower.tail = FALSE,df=2*R_j))
     
   }
-  return(list(corr_vals=rho,pvalue=pvals))
+  
+ if(K==1) print(paste0("BasCoD p-value is : ",pvals))
+ if(K!=1) print(paste0("BasCoD p-values are : ",pvals))  
+return(list(corr_vals=rho,pvalue=pvals))                    
 }
