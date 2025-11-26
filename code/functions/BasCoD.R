31b3f90212be1dad52fec4eac2a8273fe3d1416d
kmp0223 <68722153+kmp0223@users.noreply.github.com>
2025-11-26 00:16:45 -0500

    Add BasCoD_single function for correlation analysis

--- code/functions/BasCoD.R
+++ code/functions/BasCoD.R
@@ -1,5 +1,31 @@
 library(MASS)
 
+BasCoD_single=function(X_0,X_j,R_0,R_j,embedding_0,embedding_j,eps=0.3){
+  p=dim(X_0)[2]
+  embedding_0=embedding_0[,1:R_0]    
+  
+  lm0=lm(X_0~-1+embedding_0)
+  Gamma0hat=t(lm0$coefficients)
+  
+  embedding_j=embedding_j[,1:R_j]        
+  lmj=lm(X_j~-1+embedding_j)
+  Gammajhat=t(lmj$coefficients)
+  Gammajhat=apply(Gammajhat,2,function(x)x/norm(x,"2"))    
+    
+    if(R_j!=1){
+      rho=diag(cor(Gammajhat,Gamma0hat%*%ginv(Gamma0hat)%*%Gammajhat))
+    }else{rho=cor(Gammajhat,Gamma0hat%*%ginv(Gamma0hat)%*%Gammajhat)}
+    
+    
+    pval=pchisq(sum(-2*log(pnorm((atanh(rho)-atanh(1-eps))*sqrt(p-3)))),lower.tail = FALSE,df=2*R_j)
+    
+ 
+  
+print(paste0("BasCoD p-value is : ",pval))
+return(list(corr_vals=rho,pvalue=pval))                    
+}
+
+
 
 BasCoD=function(X_0,X_j_list,R_0,R_j_list,embedding_0,embedding_j_list,eps=0.3){
   p=dim(X_0)[2]
